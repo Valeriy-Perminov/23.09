eefb137cb7199b7fc712a3f5ae8c46217b0feba3
Валерий <perminov.ahura999@gmail.com>
2024-09-25 21:23:56 +0500

    Рекурсия.py

--- Рекурсия.py
+++ Рекурсия.py
@@ -4,7 +4,9 @@ def get_multiplied_digits(number):
         first = int(str_number[0])
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        return  int(str_number)
+        if int(str_number) == 0:
+            return 1
+        return int(str_number)
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(420)
 print(result)
